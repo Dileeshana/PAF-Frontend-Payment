7ba9458a33b825866d7824ab124780c19db099c2
Dileeshana <dileeshana2000@gmail.com>
2022-05-14 23:09:25 +0530

    test

---
+++ my one/payments/WebContent/Components/main.js
@@ -0,0 +1,189 @@
+ $(document).ready(function()
+{
+	if ($("#alertSuccess").text().trim() == "")
+	 {
+	 	$("#alertSuccess").hide();
+	 }
+	 $("#alertError").hide(); 
+
+}); 
+
+
+$(document).on("click", "#btnSave", function(event)
+{
+	//Clear status msges---------------------
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertError").text("");
+	$("#alertError").hide();
+	
+	//Form validation-------------------
+	var status = validateItemForm();
+	
+	//If not valid
+	if (status != true)
+	{
+		 $("#alertError").text(status);
+		 $("#alertError").show();
+		return;
+	}
+	
+	//If valid
+	$("#formPayments").submit();
+	
+	var type = ($("#hidpaymentIDSave").val() == "") ? "POST" : "PUT";
+
+	$.ajax(
+ 	{
+ 		url : "PaymentsService",
+ 		type : type,
+ 		data : $("#formPayments").serialize(),
+ 		dataType : "text",
+ 		complete : function(response, status)
+ 		{
+ 			onPaymentSaveComplete(response.responseText, status);
+ 		}
+ 	}); 
+});
+
+
+
+function onPaymentSaveComplete(response, status)
+	{
+		if (status == "success")
+		{
+			 var resultSet = JSON.parse(response);
+ 			 if (resultSet.status.trim() == "success")
+			 {
+ 				$("#alertSuccess").text("Successfully saved.");
+ 				$("#alertSuccess").show();
+ 				$("#divMeterGrid").html(resultSet.data);
+ 			 } 
+ 			 else if (resultSet.status.trim() == "error")
+			 {
+ 				$("#alertError").text(resultSet.data);
+ 				$("#alertError").show();
+ 			 }
+ 		} 
+ 		else if (status == "error")
+ 		{
+ 			$("#alertError").text("Error while saving.");
+ 			$("#alertError").show();
+ 		} 
+ 		else
+ 		{
+ 			$("#alertError").text("Unknown error while saving..");
+ 			$("#alertError").show();
+ 		}
+		$("#hidMeterIDSave").val("");
+ 		$("#formMeter")[0].reset();
+}
+
+
+// UPDATE==========================================
+$(document).on("click", ".btnUpdate", function(event)
+{
+	$("#hidItemIDSave").val($(this).data("paymentID"));
+	$("#txtAccountID").val($(this).closest("tr").find('td:eq(0)').text());
+	$("#txtAmount").val($(this).closest("tr").find('td:eq(1)').text());
+	$("#txtPayMethod").val($(this).closest("tr").find('td:eq(2)').text());
+	$("#txtPayDate").val($(this).closest("tr").find('td:eq(3)').text());
+}); 
+
+
+// REMOVE==========================================
+$(document).on("click", ".btnRemove", function(event)
+{
+	 $.ajax(
+ 		{
+ 			url : "PaymentsService",
+ 			type : "DELETE",
+ 			data : "paymentID=" + $(this).data("paymentID"),
+ 			dataType : "text",
+ 			complete : function(response, status)
+ 			{
+ 				onMeterDeleteComplete(response.responseText, status);
+ 			}
+ 		});
+}); 
+
+function onPaymentDeleteComplete(response, status)
+{
+		if (status == "success")
+ 		{
+ 			var resultSet = JSON.parse(response);
+ 			if (resultSet.status.trim() == "success")
+ 			{
+ 				$("#alertSuccess").text("Successfully deleted.");
+ 				$("#alertSuccess").show();
+ 				$("#divMeterGrid").html(resultSet.data);
+ 			} 
+ 			else if (resultSet.status.trim() == "error")
+ 			{
+ 				$("#alertError").text(resultSet.data);
+ 				$("#alertError").show();
+ 			}
+ 		} 
+ 		else if (status == "error")
+ 		{
+ 				$("#alertError").text("Error while deleting.");
+ 				$("#alertError").show();
+ 		} 
+ 		else
+ 		{
+ 				$("#alertError").text("Unknown error while deleting..");
+ 				$("#alertError").show();
+ 		}
+}
+
+
+function validateItemForm()
+{
+	//accountID
+	if ($("#txtAccountID").val().trim() == "")
+	{
+		return "Insert Account ID";
+	}
+	
+	//amount
+	if ($("#txtAmount").val().trim() == "")
+	{
+		return "Insert Amount";
+	}
+	
+	//PayMethod
+	if ($("#txtPayMethod").val() == "")
+	{
+		return "Insert Pay Method";
+	}
+	
+	//PayDate
+	if ($("#txtPayDate").val() == "")
+	{
+		return "Insert Date";
+	}
+	
+	return true;
+}
+
+
+function getPaymentCard(accountID, amount, payMethod, payDate)
+{
+	
+	var payment = "";
+	payment += "<div class=\"student card bg-light m-2\" style=\"max-width: 10rem; float: left;\">";
+	payment += "<div class=\"card-body\">";
+	payment += "accountID: " + accountID + ",";
+	payment += "<br>";
+	payment += "Amount: " + amount + ",";
+	payment += "<br>";
+	payment += "Pay Method: " + payMethod + ",";
+	payment += "<br>";
+	payment += "Date: " + payDate;
+	payment += "</div>";
+	payment += "<input type=\"button\" value=\"Remove\" class=\"btn btn-danger remove\">";
+	payment += "</div>";
+	
+	return payment;
+}
+
---
+++ my one/payments/WebContent/payments.jsp
@@ -0,0 +1,81 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+
+<link rel="stylesheet" href="Views/bootstrap.min.css">
+<script src="Components/jquery-3.2.1.min.js"></script>
+<script src="Components/main.js"></script>
+
+<title>Payments</title>
+</head>
+<body>
+
+
+	<div class="container">
+		 <div class="row">
+			 <div class="col-8">
+			 
+				 <h1 class="m-3">Payments details</h1>
+				 <form id="formPayments">
+				 
+				 	<!-- AccountID -->
+					<div class="input-group input-group-sm mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text" id="lblAccountID">Account ID: </span>
+						</div>
+						
+						<input type="text" id="txtAccountID" name="txtAccountID">
+					</div>
+					
+					<!-- Amount -->
+					<div class="input-group input-group-sm mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text" id="lblAmount">Amount: </span>
+						</div>
+						
+						<input type="text" id="txtAmount" name="txtAmount">
+					</div>
+					
+					<!-- payMethod -->
+					<div class="input-group input-group-sm mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text" id="lblPayMethod">Pay Method: </span>
+						</div>
+						
+						<input type="text" id="txtPayMethod" name="txtPayMethod" placeholder="cash or credit">
+					</div>
+					
+					<!-- payDate -->
+					<div class="input-group input-group-sm mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text" id="lblPayDate">Pay Date: </span>
+						</div>
+						
+						<input type="text" id="txtPayDate" name="txtPayDate" placeholder="yyyy-mm-dd">
+					</div>
+					
+					
+					<div id="alertSuccess" class="alert alert-success"></div>
+ 					<div id="alertError" class="alert alert-danger"></div>
+					
+					
+					<input type="button" id="btnSave" value="Save" class="btn btn-primary">
+					<input type="hidden" id="hidItemIDSave" name="hidItemIDSave" value="">
+				 
+				 </form>
+			 </div>
+		 </div>
+		
+		 <br>
+		 <div class="row">
+			 <div class="col-12" id="colPayments">
+			
+			 </div>
+		 </div>
+	</div>
+
+</body>
+</html>
